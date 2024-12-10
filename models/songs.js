e7de366300887555e2d166dd4831e4aa9e9c02a1
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2024-12-10 13:15:45 -0600

    Create songs.js

---
+++ models/songs.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+
+const Song = sequelize.define('Song', {
+  songId: { type: DataTypes.STRING, unique: true, allowNull: false },
+  title: { type: DataTypes.STRING, allowNull: false },
+  artist: { type: DataTypes.STRING, allowNull: false },
+  album: DataTypes.STRING,
+  releaseYear: DataTypes.INTEGER,
+  genre: DataTypes.STRING,
+  songUrl: DataTypes.TEXT,
+}, {
+  timestamps: true,
+});
+
+module.exports = Song;
